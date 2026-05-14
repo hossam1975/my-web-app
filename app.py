61a47549150773dfbc7d8e1b42d8bb101a656bb2
hossam1975 <salah.hassan2027@gmail.com>
2026-05-14 14:17:48 +0300

    feat: add prometheus metrics

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask
+from prometheus_client import make_wsgi_app, Counter
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 app = Flask(__name__)
 
+requests_total = Counter('http_requests_total', 'Total HTTP Requests')
+
 @app.route('/')
 def home():
+    requests_total.inc()
     return '<h1>Hello from OpenShift!</h1>'
 
 @app.route('/health')
 def health():
     return {'status': 'ok'}
+
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
